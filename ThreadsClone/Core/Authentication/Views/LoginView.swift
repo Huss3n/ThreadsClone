4681a15e396ccd61e1d0148a6872a3c03b833730
Huss3n <aisackhussein@gmail.com>
2024-02-29 16:08:54 +0300

    Project setup

---
+++ ThreadsClone/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  ThreadsClone
+//
+//  Created by Muktar Hussein on 29/02/2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Muktar Hussein on 29/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
