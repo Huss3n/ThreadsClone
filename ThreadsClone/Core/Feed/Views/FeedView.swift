13b2c6c28db975d8d182f658a8d49b4c53661748
Huss3n <aisackhussein@gmail.com>
2024-03-01 15:26:40 +0300

    Feed View UI

--- ThreadsClone/Core/Feed/Views/FeedView.swift
+++ ThreadsClone/Core/Feed/Views/FeedView.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        Text("Feed View!")
+        NavigationStack {
+            ScrollView {
+                LazyVStack {
+                    ForEach(0..<10) { _ in
+                        ThreadRow()
+                    }
+                }
+                .scrollIndicators(.hidden)
+            }
+            .refreshable {
+                print("Refresh threads")
+            }
+            .navigationTitle("Threads")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: {}, label: {
+                        Image(systemName: "arrow.counterclockwise")
+                            .foregroundStyle(.black)
+                    })
+                }
+            }
+        }
     }
 }
 
---
+++ ThreadsClone/Core/Feed/Views/ThreadRow.swift
@@ -0,0 +1,79 @@
+//
+//  ThreadRow.swift
+//  ThreadsClone
+//
+//  Created by Muktar Hussein on 01/03/2024.
+//
+
+import SwiftUI
+
+struct ThreadRow: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top, spacing: 12) {
+                Image(systemName: "person.fill")
+                    .frame(width: 40, height: 40)
+                    .overlay {
+                        Circle()
+                            .stroke(Color.black, lineWidth: 1.0)
+                    }
+                
+                VStack(alignment: .leading) {
+                    Text("Hussein Aisak")
+                        .fontWeight(.semibold)
+                    
+                    Text("Happy coding. Loving SwiftUI")
+                        .font(.footnote)
+                    
+                    HStack(spacing: 12) {
+                        Button(action: {
+                            
+                        }, label: {
+                            Image(systemName: "heart")
+                        })
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            Image(systemName: "bubble.right")
+                        })
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            Image(systemName: "arrow.rectanglepath")
+                        })
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            Image(systemName: "paperplane")
+                        })
+                    }
+                    .foregroundStyle(.black)
+                    .padding(.vertical, 8)
+                }
+                
+                Spacer()
+                
+                Text("10m")
+                    .font(.caption)
+                    .foregroundStyle(Color(.systemGray3))
+                
+                Button(action: {}, label: {
+                    Image(systemName: "ellipsis")
+                        .foregroundStyle(Color(.darkGray))
+                })
+                
+            }
+            .padding(.leading)
+            
+            Divider()
+        }
+        .padding(4)
+    }
+}
+
+#Preview {
+    ThreadRow()
+}
