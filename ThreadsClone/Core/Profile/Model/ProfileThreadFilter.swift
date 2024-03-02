ced02c530d189285322530135df8fed325bdbd09
Huss3n <aisackhussein@gmail.com>
2024-03-02 20:18:59 +0300

    Threads enum

---
+++ ThreadsClone/Core/Profile/Model/ProfileThreadFilter.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileThreadFilter.swift
+//  ThreadsClone
+//
+//  Created by Muktar Hussein on 02/03/2024.
+//
+
+import Foundation
--- ThreadsClone/Core/Profile/Views/ProfileViews.swift
+++ ThreadsClone/Core/Profile/Views/ProfileViews.swift
@@ -9,7 +9,53 @@ import SwiftUI
 
 struct ProfileViews: View {
     var body: some View {
-        Text("Profile")
+        ScrollView {
+            VStack(alignment: .leading, spacing: 4) {
+                // bio and stats
+                VStack(spacing: 20) {
+                    HStack(alignment: .top) {
+                        VStack(alignment: .leading, spacing: 12) {
+                            // fullname and username
+                            Text("Muktar Hussein")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                            
+                            Text("hussein_aisak")
+                                .font(.subheadline)
+                            
+                            Text("iOS Developer exploring the tech world")
+                                .font(.caption)
+                            
+                            Text("10 followers")
+                                .font(.caption)
+                                .foregroundStyle(.gray)
+                        }
+                        
+                        Spacer()
+                        ProfileImageView()
+                        
+                    }
+                    .padding(.horizontal)
+                }
+                
+                // edit and share buttons
+                HStack {
+                    Button(action: {}, label: {
+                        Text("Follow")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.white)
+                            .frame(width: 352, height: 32)
+                            .background(.black)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                    })
+                }
+                
+                // user content list view
+                
+                
+            }
+        }
     }
 }
 
