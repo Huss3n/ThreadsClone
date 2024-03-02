03c96ec0be3fad4f64237bb70c86095c721a993f
Huss3n <aisackhussein@gmail.com>
2024-03-02 21:38:05 +0300

    Create thread UI

--- ThreadsClone/Core/UploadThread/Views/CreateThread.swift
+++ ThreadsClone/Core/UploadThread/Views/CreateThread.swift
@@ -8,8 +8,62 @@
 import SwiftUI
 
 struct CreateThread: View {
+    @State private var thread: String = ""
     var body: some View {
-        Text("Upload View")
+        NavigationStack {
+            VStack{
+                
+                HStack(alignment: .top) {
+                    ProfileImageView()
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Hussein Aisak")
+                            .fontWeight(.semibold)
+                        
+                        TextField("Start a thread", text: $thread)
+                    }
+                    .font(.footnote)
+                    
+                    Spacer()
+                    
+                    if !thread.isEmpty {
+                        Button(action: {
+                            thread = ""
+                        }, label: {
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 12, height: 12)
+                                .foregroundStyle(.gray)
+                        })
+                    }
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Cancel") {
+                        //
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(.black)
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Post") {
+                        //
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(.black)
+                    .fontWeight(.semibold)
+                    .opacity(thread.isEmpty ? 0.5 : 1.0)
+                    .disabled(thread.isEmpty)
+                }
+            }
+            .navigationTitle("New Thread")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
