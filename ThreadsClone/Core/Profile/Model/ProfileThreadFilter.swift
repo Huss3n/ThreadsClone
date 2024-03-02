92cbe13a877737ea4fff188ae9b7686ff7d6ffe6
Huss3n <aisackhussein@gmail.com>
2024-03-02 20:44:17 +0300

    Profile UI complete

--- ThreadsClone/Core/Profile/Model/ProfileThreadFilter.swift
+++ ThreadsClone/Core/Profile/Model/ProfileThreadFilter.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+enum ProfileThreadFilter: Int, CaseIterable, Identifiable {
+    case threads
+    case replies
+//    case reposts
+    
+    var title: String {
+        switch self {
+        case .threads:
+            return "Threads"
+        case .replies:
+            return "Replies"
+//        case .reposts:
+//            return "Reposts"
+        }
+    }
+    
+    var id: Int { return self.rawValue }
+}
--- ThreadsClone/Core/Profile/Views/ProfileViews.swift
+++ ThreadsClone/Core/Profile/Views/ProfileViews.swift
@@ -8,11 +8,19 @@
 import SwiftUI
 
 struct ProfileViews: View {
+    @State private var selectedFilter: ProfileThreadFilter = .threads
+    @Namespace var animation
+    
+    private var tabBarWidth: CGFloat {
+        let count = CGFloat(ProfileThreadFilter.allCases.count)
+        return UIScreen.main.bounds.width / count - 10
+    }
+    
     var body: some View {
         ScrollView {
-            VStack(alignment: .leading, spacing: 4) {
+            VStack(spacing: 4) {
                 // bio and stats
-                VStack(spacing: 20) {
+                VStack(spacing: 40) {
                     HStack(alignment: .top) {
                         VStack(alignment: .leading, spacing: 12) {
                             // fullname and username
@@ -52,8 +60,40 @@ struct ProfileViews: View {
                 }
                 
                 // user content list view
-                
-                
+                VStack {
+                    HStack {
+                        ForEach(ProfileThreadFilter.allCases){ filter in
+                            VStack {
+                                Text(filter.title)
+                                    .font(.subheadline)
+                                    .fontWeight(selectedFilter == filter ? .semibold : .regular)
+                                
+                                if selectedFilter == filter {
+                                    Rectangle()
+                                        .foregroundStyle(.black)
+                                        .frame(width: tabBarWidth, height: 1)
+                                        .matchedGeometryEffect(id: "item", in: animation )
+                                } else {
+                                    Rectangle()
+                                        .foregroundStyle(.clear)
+                                        .frame(width: tabBarWidth, height: 1)
+                                }
+                            }
+                            .onTapGesture {
+                                withAnimation(.spring) {
+                                    selectedFilter = filter
+                                }
+                            }
+                        }
+                    }
+                    
+                    LazyVStack {
+                        ForEach(0..<20){ _ in
+                            ThreadRow()
+                        }
+                    }
+                }
+                .padding(.vertical, 8)
             }
         }
     }
