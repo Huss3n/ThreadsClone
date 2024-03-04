6e29f9c994ee420771888da9add0ee557877b91f
Huss3n <aisackhussein@gmail.com>
2024-03-04 11:53:06 +0300

    Settings view

--- ThreadsClone/Core/Profile/Views/ProfileViews.swift
+++ ThreadsClone/Core/Profile/Views/ProfileViews.swift
@@ -17,85 +17,96 @@ struct ProfileViews: View {
     }
     
     var body: some View {
-        ScrollView {
-            VStack(spacing: 4) {
-                // bio and stats
-                VStack(spacing: 40) {
-                    HStack(alignment: .top) {
-                        VStack(alignment: .leading, spacing: 12) {
-                            // fullname and username
-                            Text("Muktar Hussein")
-                                .font(.title2)
-                                .fontWeight(.semibold)
-                            
-                            Text("hussein_aisak")
-                                .font(.subheadline)
-                            
-                            Text("iOS Developer exploring the tech world")
-                                .font(.caption)
-                            
-                            Text("10 followers")
-                                .font(.caption)
-                                .foregroundStyle(.gray)
-                        }
-                        
-                        Spacer()
-                        ProfileImageView()
-                        
-                    }
-                    .padding(.horizontal)
-                }
-                
-                // edit and share buttons
-                HStack {
-                    Button(action: {}, label: {
-                        Text("Follow")
-                            .font(.subheadline)
-                            .fontWeight(.semibold)
-                            .foregroundStyle(.white)
-                            .frame(width: 352, height: 32)
-                            .background(.black)
-                            .clipShape(RoundedRectangle(cornerRadius: 8))
-                    })
-                }
-                
-                // user content list view
-                VStack {
-                    HStack {
-                        ForEach(ProfileThreadFilter.allCases){ filter in
-                            VStack {
-                                Text(filter.title)
-                                    .font(.subheadline)
-                                    .fontWeight(selectedFilter == filter ? .semibold : .regular)
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 4) {
+                    // bio and stats
+                    VStack(spacing: 40) {
+                        HStack(alignment: .top) {
+                            VStack(alignment: .leading, spacing: 12) {
+                                // fullname and username
+                                Text("Muktar Hussein")
+                                    .font(.title2)
+                                    .fontWeight(.semibold)
                                 
-                                if selectedFilter == filter {
-                                    Rectangle()
-                                        .foregroundStyle(.black)
-                                        .frame(width: tabBarWidth, height: 1)
-                                        .matchedGeometryEffect(id: "item", in: animation )
-                                } else {
-                                    Rectangle()
-                                        .foregroundStyle(.clear)
-                                        .frame(width: tabBarWidth, height: 1)
-                                }
+                                Text("hussein_aisak")
+                                    .font(.subheadline)
+                                
+                                Text("iOS Developer exploring the tech world")
+                                    .font(.caption)
+                                
+                                Text("10 followers")
+                                    .font(.caption)
+                                    .foregroundStyle(.gray)
                             }
-                            .onTapGesture {
-                                withAnimation(.spring) {
-                                    selectedFilter = filter
-                                }
-                            }
+                            
+                            Spacer()
+                            ProfileImageView()
+                            
                         }
+                        .padding(.horizontal)
                     }
                     
-                    LazyVStack {
-                        ForEach(0..<20){ _ in
-                            ThreadRow()
+                    // edit and share buttons
+                    HStack {
+                        Button(action: {}, label: {
+                            Text("Follow")
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .foregroundStyle(.white)
+                                .frame(width: 352, height: 32)
+                                .background(.black)
+                                .clipShape(RoundedRectangle(cornerRadius: 8))
+                        })
+                    }
+                    
+                    // user content list view
+                    VStack {
+                        HStack {
+                            ForEach(ProfileThreadFilter.allCases){ filter in
+                                VStack {
+                                    Text(filter.title)
+                                        .font(.subheadline)
+                                        .fontWeight(selectedFilter == filter ? .semibold : .regular)
+                                    
+                                    if selectedFilter == filter {
+                                        Rectangle()
+                                            .foregroundStyle(.black)
+                                            .frame(width: tabBarWidth, height: 1)
+                                            .matchedGeometryEffect(id: "item", in: animation )
+                                    } else {
+                                        Rectangle()
+                                            .foregroundStyle(.clear)
+                                            .frame(width: tabBarWidth, height: 1)
+                                    }
+                                }
+                                .onTapGesture {
+                                    withAnimation(.spring) {
+                                        selectedFilter = filter
+                                    }
+                                }
+                            }
+                        }
+                        
+                        LazyVStack {
+                            ForEach(0..<20){ _ in
+                                ThreadRow()
+                            }
+                        }
+                    }
+                    .padding(.vertical, 8)
+                }
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        NavigationLink {
+                            Text("Settings view")
+                        } label: {
+                            Image(systemName: "line.3.horizontal")
+                                .foregroundStyle(.black)
                         }
                     }
                 }
-                .padding(.vertical, 8)
             }
-           
         }
     }
 }
