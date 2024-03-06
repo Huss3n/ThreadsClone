71418652844adb75bd9fea057b35f32204392dfe
Huss3n <aisackhussein@gmail.com>
2024-03-06 23:35:52 +0300

    User data model

---
+++ ThreadsClone/Models/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  ThreadsClone
+//
+//  Created by Muktar Hussein on 06/03/2024.
+//
+
+import Foundation
+
+
+// user data model
+struct User: Identifiable, Codable {
+    let id: String
+    let fullName: String
+    let email: String
+    let username: String
+    let profileImageURL: String?
+    let bio: String?
+}
