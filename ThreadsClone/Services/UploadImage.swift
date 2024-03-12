4c287d5c96fdce779f28492c42365832f0c40658
Huss3n <aisackhussein@gmail.com>
2024-03-12 16:50:40 +0300

    Image upload service

--- ThreadsClone/Services/UploadImage.swift
+++ ThreadsClone/Services/UploadImage.swift
@@ -6,7 +6,29 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseStorage
 
-class UploadImage {
-    
+
+struct UploadImage {
+    static func uploadImage(_ image: UIImage) async throws -> String? {
+        // 1. compress the image quality
+        guard let imageData = image.jpegData(compressionQuality: 0.25) else { return nil }
+        
+        // 2. create a unique id for the image
+        let filename = UUID().uuidString
+        
+        // 3. create a storage ref
+        let storageRef = Storage.storage().reference(withPath: "/profile_images/\(filename)")
+        
+        // save the image to storage
+        do {
+            let _ = try await storageRef.putDataAsync(imageData)
+            let imageURL = try await storageRef.downloadURL()
+            return imageURL.absoluteString
+        } catch {
+            print("Error saving image to storage \(error.localizedDescription)")
+            return nil
+        }
+    }
 }
